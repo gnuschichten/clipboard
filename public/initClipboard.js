fd9a2b5ce748923cf23e8ca51919d1ec162c25df
Holger Weischenberg <holger@drupalnoob.de>
2021-03-13 11:18:23 +0100

    add conditional to avoid var null output on pages

--- public/initClipboard.js
+++ public/initClipboard.js
@@ -1,23 +1,29 @@
 const items = document.querySelectorAll('pre');
+const initButton = document.getElementsByTagName('code');
 
-for (const item of items) {
+if (initButton) {
+    for (const item of items) {
 
-    const button = document.createElement("button");
-    const content = item.innerText;
+        const button = document.createElement("button");
+        const content = item.innerText;
 
-    button.innerHTML = "copy";
-    button.classList.add("btn");
-    button.setAttribute('data-clipboard-text', content);
+        button.innerHTML = "copy";
+        button.classList.add("btn");
+        button.setAttribute('data-clipboard-text', content);
 
-    item.appendChild(button);
+        item.appendChild(button);
 
+    }
+
+    const buttons = document.querySelector('.btn');
+
+    if (buttons) {
+
+        buttons.addEventListener("click", function () {
+
+            const btns = document.querySelectorAll('.btn');
+            const clipboard = new ClipboardJS(btns);
+
+        });
+    }
 }
-
-const buttons = document.querySelector('.btn');
-
-buttons.addEventListener("click", function () {
-
-    const btns = document.querySelectorAll('.btn');
-    const clipboard = new ClipboardJS(btns);
-    
-});
